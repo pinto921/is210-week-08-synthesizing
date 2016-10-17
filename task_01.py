14758fd8ca7db46810a431c92a35bac1476d007a
pinto921 <pinto921@hotmail.com>
2016-10-17 03:20:00 +0000

    task 1

---
+++ task_01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+""" task 1 """
+
+BASE = raw_input('Which base color "Seattle gray" or "Manatee"?')
+
+if BASE is 'Seatttle gray':
+    ACCENT = raw_input ('which accent color, "Ceramic Glaze" or "Cumulus Nimbus"?')
+
+    if ACCENT is 'Ceramic Glaze':
+        HIGHLIGHT =raw_input ('which highlight color, "Basically white" or "White White"?')
+
+else:
+    ACCENT = raw_input('WHich accent color, "Platinum Mist" or "Spartan Sage"')
+
+    if ACCENT is 'Platinum Mist'
+    HIGHLIGHT = raw_input('Which highlight color, "Bone white" or "Just White"?')
+
+    else:
+        HIGHLIGHT = raw_input ('Which highlight color, "Bone white or "not White"?')
+FINAL_RESULT = 'your selected colors are, {}, {} and {}'.\
+          format(BASE, ACCCENT, HIGHLIGHT)
+
+print FINAL_RESULT
+
+
+
+
+
+
+                       
+    
